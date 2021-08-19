9fe35729ff965dec9890ec4e8db0ab312ff44ff6
Akhil132-dev <agiri5375@gmail.com>
2021-08-19 17:40:25 +0530

    add userRouter

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import authRoute from './Routes/auth.js'
+import userRoute from './Routes/user.js'
 const app = express();
 
 dotenv.config();
@@ -14,9 +15,11 @@ app.use(express.json())
 mongoose.connect(process.env.MONGO_URL ,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    useCreateIndex:true
+    useCreateIndex:true,
+    useFindAndModify:true
 }).then(()=>console.log("connneted to mongodb")).catch((err)=>console.log(err))
 app.use('/api/auth',authRoute)
+app.use('/api/user',userRoute)
 
 const port = process.env.PORT || 9099;
 app.listen(port, () => console.log(`Listening on ${port}`));
