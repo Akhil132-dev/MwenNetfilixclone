0ba8daf4b8dda9b04658a66a4e68a9decf74a9d6
Akhil132-dev <agiri5375@gmail.com>
2021-08-21 10:40:34 +0530

    backend for moives has complelted

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,12 +1,21 @@
 import express from 'express'
-import { updateUser } from '../Controller/user.js';
+import { DeleteUser, getAllUser, getState, getUser, updateUser } from '../Controller/user.js';
+import User from '../model/user.js';
 import verify from '../verifyToken.js';
 const router = express.Router();
 /** @dev update the password of the user  */
 router.put('/:id',verify,updateUser)
 
 /** @dev delete the password of the user  */
+router.delete('/:id',verify,DeleteUser)
 /** @dev getall user    */
-/** @dev get on user   */
+router.get('/',verify,getAllUser)
+
+/** @dev get one user   */
+router.get("/find/:id",getUser)
+ 
+
+  
 /** @dev get use State   */
+router.get('/state',getState)
 export default router
